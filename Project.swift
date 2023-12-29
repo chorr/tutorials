d8543329b8c7b60d7cae330d2c300f4e6fbd44e5
Jeon Hyuncheol <chorr@imgbase.me>
2023-12-29 13:12:27 +0900

    first commit

---
+++ Project.swift
@@ -0,0 +1,24 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Tutorials",
+    settings: .settings(
+        defaultSettings: .recommended
+    ),
+    targets: [
+        Target(
+            name: "TCATutorial",
+            destinations: [.iPhone, .iPad, .macWithiPadDesign],
+            product: .app,
+            bundleId: "net.chorr.TCATutorial",
+            deploymentTargets: .iOS("17.0"),
+            infoPlist: .default,
+            sources: [
+                "Targets/TCATutorial/Sources/**"
+            ],
+            resources: [
+                "Targets/TCATutorial/Resources/**"
+            ]
+        )
+    ]
+)
