ebcd60ac7f5d4a435eda0499fa9aa2ea7208511d
Jeon Hyuncheol <chorr@imgbase.me>
2024-01-02 19:11:16 +0900

    ✨ Your first presentation

---
+++ Targets/TCATutorial/Sources/AddContactView.swift
@@ -0,0 +1,41 @@
+import ComposableArchitecture
+import SwiftUI
+
+struct AddContactView: View {
+    let store: StoreOf<AddContactFeature>
+
+    var body: some View {
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            Form {
+                TextField("Name", text: viewStore.binding(get: \.contact.name, send: { .setName($0) }))
+                Button("Save") {
+                    viewStore.send(.saveButtonTapped)
+                }
+            }
+            .toolbar {
+                ToolbarItem {
+                    Button("Cancel") {
+                        viewStore.send(.cancelButtonTapped)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AddContactView(
+            store: Store(
+                initialState: AddContactFeature.State(
+                    contact: Contact(
+                        id: UUID(),
+                        name: "Blob"
+                    )
+                )
+            ) {
+                AddContactFeature()
+            }
+        )
+    }
+}
---
+++ Targets/TCATutorial/Sources/ContactsView.swift
@@ -0,0 +1,54 @@
+import ComposableArchitecture
+import SwiftUI
+
+struct ContactsView: View {
+    let store: StoreOf<ContactsFeature>
+
+    var body: some View {
+        NavigationStack {
+            WithViewStore(store, observe: \.contacts) { viewStore in
+                List {
+                    ForEach(viewStore.state) { contact in
+                        Text(contact.name)
+                    }
+                }
+                .navigationTitle("Contacts")
+                .toolbar {
+                    ToolbarItem {
+                        Button {
+                            viewStore.send(.addButtonTapped)
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                }
+            }
+        }
+        .sheet(
+            store: store.scope(
+                state: \.$addContact,
+                action: \.addContact
+            )
+        ) { addContactStore in
+            NavigationStack {
+                AddContactView(store: addContactStore)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr"),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
+}
