0ada2ee7177e6083657f71b731549b5c59955e7b
Jeon Hyuncheol <chorr@imgbase.me>
2024-01-02 16:48:20 +0900

    ✨ 테스트 코드 마무리

--- Targets/TCATutorial/Sources/CounterFeature.swift
+++ Targets/TCATutorial/Sources/CounterFeature.swift
@@ -23,6 +23,9 @@ struct CounterFeature {
         case timer
     }
 
+    @Dependency(\.continuousClock) var clock
+    @Dependency(\.numberFact) var numberFact
+
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
@@ -40,10 +43,7 @@ struct CounterFeature {
                 state.fact = nil
                 state.isLoading = true
                 return .run { [count = state.count] send in
-                    let (data, _) = try await URLSession.shared
-                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
-                    let fact = String(decoding: data, as: UTF8.self)
-                    await send(.factResponse(fact))
+                    try await send(.factResponse(self.numberFact.fetch(count)))
                 }
 
             case let .factResponse(fact):
@@ -55,7 +55,7 @@ struct CounterFeature {
                 state.isTimerRunning.toggle()
                 if state.isTimerRunning {
                     return .run { send in
-                        while true {
+                        for await _ in self.clock.timer(interval: .seconds(1)) {
                             try await Task.sleep(for: .seconds(1))
                             await send(.timerTick)
                         }
